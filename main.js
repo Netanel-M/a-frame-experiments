fc5f8c299d6d18cee2a890ece85788fcf9e073b1
nand <nand@nanlap.home>
2024-02-20 01:19:23 +0200

    bump

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ AFRAME.registerComponent('fall', {
 
 AFRAME.registerComponent('plane-follower', {
   tick: function (t, dt) {
-    const handPosition = document.querySelector('#leftHand').object3D("mesh");
+      const handPosition = document.querySelector('#leftHand').object3D.mesh;
     console.log(handPosition.position);
     const planePosition = this.el.object3D.position;
     planePosition.copy(handPosition); // Use this line to place the plane directly on the hand
